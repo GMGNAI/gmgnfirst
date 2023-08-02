c30413bba664796045b03d13acbe968fa0da8346
GMGN.AI <141223802+GMGNAI@users.noreply.github.com>
2023-08-02 17:20:48 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: "",
+});
+(async () => {
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Hello world",
+  });
+  console.log(completion.data.choices[0].text);
+})();
